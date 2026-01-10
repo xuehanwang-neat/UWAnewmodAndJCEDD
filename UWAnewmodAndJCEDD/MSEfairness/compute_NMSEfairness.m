b20a6a32b0093831e1cd90884f0a024fa0794d87
xuehanwang-neat <wang-xh21@mails.tsinghua.edu.cn>
2026-01-10 10:02:18 +0800

    Delete UWAnewmodAndJCEDD directory

--- UWAnewmodAndJCEDD/MSEfairness/compute_NMSEfairness.m
+++
@@ -1,6 +0,0 @@
-function nmse_result = compute_NMSEfairness(eh)
-N = length(eh) ;
-eh_opt = sum(eh) / N * ones(N,1) ;
-nmse_result_norm = norm(eh_opt-eh,2) / norm(eh_opt,2) ;
-nmse_result = nmse_result_norm*nmse_result_norm ;
-
